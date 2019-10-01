c46dd780c1eabf3afd2c53efa56bff42bf173a30
Richard Riley <rileyrg@gmail.com>
2019-10-01 22:25:32 +0200

    some defines

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
-/*
- * Author: MIUZEI
- * IDE V1.6.9
- * Email:support@miuzeipro.com
- */
 #include <Arduino.h>
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+#define STEPSPERREVOLUTION 4096
 // for your motor
 // initialize the stepper library on pins 2 through 5:
-Stepper myStepper(stepsPerRevolution, 2, 3, 4, 5);
+Stepper myStepper(STEPSPERREVOLUTION, 2, 3, 4, 5);
 int stepCount = 0;         // number of steps the motor has taken
 void setup() {
   // initialize the serial port:
-  Serial.begin(9600);
+  // Serial.begin(9600);
 }
 void loop() {
   // step one step:
   myStepper.step(1);
-  Serial.print("steps:");
-  Serial.println(stepCount);
+  // Serial.print("steps:");
+  // Serial.println(stepCount);
   stepCount++;
-  delay(500);
+  delay(10);
 }
